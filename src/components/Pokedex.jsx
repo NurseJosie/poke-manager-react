11c25d5cbb0c76f84633f10c0adb22b0fc277c9f
Josefin Persson <josefinp94@gmail.com>
2022-04-13 11:15:13 +0200

    fetch data

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -2,20 +2,23 @@ import { useState, useEffect } from 'react';
 import './Pokedex.css';
 
 const Pokedex = () => {
-  const [pokemon, setPokemon] = useState({});
+  const [pokemon, setPokemon] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [pokemonNum, setPokemonNum] = useState(0);
 
   //https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
   const getData = async () => {
     try {
       setIsFetching(true);
-      const url = `https://pokeapi.co/api/v2/pokemon?limit=40&offset=0`;
+      const url = `https://pokeapi.co/api/v2/pokemon?limit=40&offset=${pokemonNum}`;
       console.log('About to send request: ', url);
       const response = await fetch(url);
       const data = await response.json();
 
       console.log('Response from API: ', data);
-      setPokemon(data.results.pokemon);
+      setPokemon(data.results);
+      console.log(pokemon);
+      setPokemonNum(pokemonNum + 40);
       setIsFetching(false);
     } catch {
       console.log('error');
@@ -31,12 +34,14 @@ const Pokedex = () => {
       <h3>POKÉDEX</h3>
       <input type="text" placeholder="Search.." />
       <div className="result-box">
-        {pokemon.length > 0 && (
-          <ul>
-            {pokemon.map((p) => (
-              <li key={pokemon.name}>{pokemon.name}</li>
-            ))}
-          </ul>
+        {isFetching === false ? (
+          pokemon.map((pokemon) => (
+            <ul key={pokemon.name}>
+              <li>{pokemon.name}</li>
+            </ul>
+          ))
+        ) : (
+          <span>Your pokemon are getting catched!</span>
         )}
       </div>
       <button className="next-button" onClick={getData}>
